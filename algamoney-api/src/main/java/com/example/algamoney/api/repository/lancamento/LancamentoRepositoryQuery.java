380d134a962a84f805a9f05bdca2559761ed1df2
nog <nog.guitar@gmail.com>
2019-10-10 17:47:11 -0300

    Implementing projections

--- algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/repository/lancamento/LancamentoRepositoryQuery.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 
 import com.example.algamoney.api.model.Lancamento;
 import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.repository.projection.ResumoLancamento;
 
 /**
  * @author andersonnogueira
@@ -15,4 +16,5 @@ import com.example.algamoney.api.repository.filter.LancamentoFilter;
  */
 public interface LancamentoRepositoryQuery {
 	public Page<Lancamento> filtrar(LancamentoFilter filter, Pageable pageable);
+	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
