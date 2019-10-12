9efde8532c006c5ac967477d893db1c1fe3eddf0
nog <nog.guitar@gmail.com>
2019-10-12 01:49:57 -0300

    Searching for "Pessoa" by Name

--- algamoney-api/src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
+++ algamoney-api/src/main/java/com/example/algamoney/api/repository/PessoaRepository.java
@@ -3,6 +3,8 @@
  */
 package com.example.algamoney.api.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Pessoa;
@@ -12,5 +14,7 @@ import com.example.algamoney.api.model.Pessoa;
  * @since 2 de out de 2019
  */
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+
+	Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
